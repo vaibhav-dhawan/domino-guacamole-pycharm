4ef7f57eef7a393ad8562cac330408b08501e406
Vaibhav Dhawan <vaibhav.dhawan@dominodatalab.com>
2021-09-07 14:31:14 +0100

    Initial commit

---
+++ supporting/rdp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Load Domino environment variables
+source $HOME/.domino-defaults
+source $HOME/.rdp-defaults
+
+# Configure Fluxbox
+mkdir $HOME/.fluxbox
+#echo "session.screen0.toolbar.visible: false" > $HOME/.fluxbox/init
+echo "session.screen0.toolbar.tools:  iconbar, systemtray" > $HOME/.fluxbox/init
+echo "session.screen0.titlebar.left:  Stick" >> $HOME/.fluxbox/init 
+echo "session.screen0.titlebar.right: Maximize" >> $HOME/.fluxbox/init
+echo "background: none" > $HOME/.fluxbox/overlay
+
+# Launch Texmaker
+cd $DOMINO_WORKING_DIR
+fluxbox &
+/opt/pycharm-community-2021.2.1/bin/pycharm.sh
